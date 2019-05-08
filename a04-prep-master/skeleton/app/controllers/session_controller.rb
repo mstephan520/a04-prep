551ecb334df96c26b686485eb9b465bfef947d93
App Academy Student <appacademy>
2019-05-07 22:16:38 -0400

    Initial commit

---
+++ a04-prep-master/skeleton/app/controllers/session_controller.rb
@@ -0,0 +1,25 @@
+class SessionController < ApplicationController
+
+    def new
+    end
+        
+    def create
+        user = User.find_by_credentials(params[:user][:username],
+                                        params[:user][:password])
+
+        if user
+            login(user)
+            redirect_to links_url
+        else
+            flash.now[:errors] = ['Invalid username or password']
+            render :new
+        end
+    end
+
+    def destroy
+        log_out
+        redirect_to new_session_url
+    end
+
+
+end
